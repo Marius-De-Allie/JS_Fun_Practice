d55ecc1dd194d53c904262cb6394e24f18d8916c
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-21 12:39:05 -0400

    Declare subb function

--- Solutions/my-solutions-MD.js
+++ Solutions/my-solutions-MD.js
@@ -1,3 +1,16 @@
 // identity function.
 
 const identity = x => x;
+
+// subb function.
+
+const subb = (a, b) =>{
+    // Check whether arguments are of type number.
+    if(typeof a !== 'number' && typeof b !== 'number') {
+        // alert('please enter two integers as arguments to the function.')
+        console.log('please enter two integers as arguments to the function.')
+        // If args are of type number then perform calculation
+    } else {
+        return a - b;
+    }
+};
